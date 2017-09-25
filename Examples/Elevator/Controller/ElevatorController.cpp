c7c47e023decf84dd2f47f9fe31714c75d382c40
Eduardo Valgôde <vldtecno@sapo.pt>
2017-09-25 17:38:22 +0200

    Controller development

--- Examples/Elevator/Controller/ElevatorController.cpp
+++ Examples/Elevator/Controller/ElevatorController.cpp
@@ -21,11 +21,18 @@
 #include "Controller/ElevatorPetriNet.h"
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 ElevatorController::ElevatorController():
-	m_pPetriNet{nullptr}
+	m_pPetriNet{nullptr},
+	m_currentFloor(s_bottomFloor),
+	m_destinationFloor(s_bottomFloor),
+	m_upCallerFloor(s_bottomFloor),
+	m_downCallerFloor(s_bottomFloor),
+	m_goingUp(false),
+	m_goingDown(false)
 {
 
 }
@@ -47,12 +54,20 @@ void ElevatorController::initialize()
 
 void ElevatorController::openDoors()
 {
-
+	if (!m_pPetriNet)
+	{
+		throw runtime_error("Petri net not available.");
+	}
+	m_pPetriNet->openDoors();
 }
 
 void ElevatorController::closeDoors()
 {
-
+	if (!m_pPetriNet)
+	{
+		throw runtime_error("Petri net not available.");
+	}
+	m_pPetriNet->closeDoors();
 }
 
 
@@ -62,6 +77,12 @@ bool ElevatorController::callElevatorUp(const int floor)
 	{
 		return false;
 	}
+	if (!m_pPetriNet)
+	{
+		throw runtime_error("Petri net not available.");
+	}
+	m_upCallerFloor = floor;
+	m_pPetriNet->callButtonUp();
 	return true;
 }
 
@@ -71,6 +92,12 @@ bool ElevatorController::callElevatorDown(const int floor)
 	{
 		return false;
 	}
+	if (!m_pPetriNet)
+	{
+		throw runtime_error("Petri net not available.");
+	}
+	m_downCallerFloor = floor;
+	m_pPetriNet->callButtonDown();
 	return true;
 }
 
@@ -80,14 +107,238 @@ bool ElevatorController::setDestinationFloor(const int floor)
 	{
 		return false;
 	}
+	if (!m_pPetriNet)
+	{
+		throw runtime_error("Petri net not available.");
+	}
+	m_destinationFloor = floor;
+	m_pPetriNet->destinationButton();
 	return true;
 }
 
 
 
+bool ElevatorController::dlCallElevatorUp(const int floor)
+{
+	bool addedDestination = false;
+	if (m_goingUp && !m_goingDown)
+	{
+		if (floor > m_currentFloor)
+		{
+			addedDestination = addDestination1(floor);
+		}
+		else if (floor < m_currentFloor)
+		{
+			addedDestination = addWaitingToGoUp(floor);
+		}
+	}
+	else if (m_goingDown && !m_goingUp)
+	{
+		addedDestination = addDestination2(floor);
+	}
+	else if (!m_goingUp && !m_goingDown)
+	{
+		addedDestination = addDestination1(floor);
+	}
+	else
+	{
+		throw runtime_error("Messed up.");
+	}
+	return addedDestination;
+
+}
+
+bool ElevatorController::dlCallElevatorDown(const int floor)
+{
+	bool addedDestination = false;
+	if (m_goingDown && !m_goingUp)
+	{
+		if (floor < m_currentFloor)
+		{
+			addedDestination = addDestination1(floor);
+		}
+		else if (floor > m_currentFloor)
+		{
+			addedDestination = addWaitingToGoDown(floor);
+		}
+	}
+	else if (m_goingUp && !m_goingDown)
+	{
+		addedDestination = addDestination2(floor);
+	}
+	else if (!m_goingUp && !m_goingDown)
+	{
+		addedDestination = addDestination1(floor);
+	}
+	else
+	{
+		throw runtime_error("Messed up.");
+	}
+	return addedDestination;
+
+}
+
+
+bool ElevatorController::dlSetDestinationFloor(const int floor)
+{
+	bool addedDestination = false;
+	if (m_goingUp && !m_goingDown)
+	{
+		if (floor > m_currentFloor)
+		{
+			addedDestination = addDestination1(floor);
+		}
+		else if (floor < m_currentFloor)
+		{
+			addedDestination = addDestination2(floor); //no replace
+		}
+	}
+	else if (m_goingDown && !m_goingUp)
+	{
+		if (floor < m_currentFloor)
+		{
+			addedDestination = addDestination1(floor);
+		}
+		else if (floor > m_currentFloor)
+		{
+			addedDestination = addDestination2(floor); //no replace
+		}
+	}
+	else if (!m_goingUp && !m_goingDown)
+	{
+		addedDestination = addDestination1(floor);
+	}
+	else
+	{
+		throw runtime_error("Messed up.");
+	}
+	return addedDestination;
+
+}
+
+void ElevatorController::dlArrivedFloor(const int floor)
+{
+	m_destinations1.erase(floor);
+	if (list1IsEmpty())
+	{
+		m_endTravel = true;
+	}
+}
+
+
+void ElevatorController::dlGoUp()
+{
+	m_goingUp = true;
+	m_endTravel = false;
+
+	if (list1IsEmpty())
+	{
+		rotateLists();
+	}
+
+	mergeWaitingToGoUp();
+	mergeMaxWaitingToGoDown();
 
 
 
+}
 
+void ElevatorController::dlGoDown()
+{
+	m_goingDown = true;
+	m_endTravel = false;
+
+	if (list1IsEmpty())
+	{
+		rotateLists();
+	}
+
+	mergeWaitingToGoDown();
+	mergeMinWaitingToGoUp();
+
+
+}
+
+bool ElevatorController::addDestination1(const int floor)
+{
+	if (m_destinations1.find(floor) != m_destinations1.end())
+	{
+		m_destinations1.insert(floor);
+		return true;
+	}
+	return false;
+}
+
+bool ElevatorController::addDestination2(const int floor)
+{
+	if (m_destinations2.find(floor) != m_destinations2.end())
+	{
+		m_destinations2.insert(floor);
+		return true;
+	}
+	return false;
+}
+
+bool ElevatorController::addWaitingToGoDown(const int floor)
+{
+	if (m_waitingToGoDown.find(floor) != m_waitingToGoDown.end())
+	{
+		m_waitingToGoDown.insert(floor);
+		return true;
+	}
+	return false;
+}
+
+bool ElevatorController::addWaitingToGoUp(const int floor)
+{
+	if (m_waitingToGoUp.find(floor) != m_waitingToGoUp.end())
+	{
+		m_waitingToGoUp.insert(floor);
+		return true;
+	}
+	return false;
+}
+
+bool ElevatorController::list1IsEmpty() const
+{
+	return m_destinations1.empty();
+}
+
+void ElevatorController::rotateLists()
+{
+	swap(m_destinations1, m_destinations2);
+}
+
+void ElevatorController::mergeWaitingToGoUp()
+{
+	m_destinations1.insert(m_waitingToGoUp.begin(), m_waitingToGoUp.end());
+}
+
+void ElevatorController::mergeWaitingToGoDown()
+{
+	m_destinations1.insert(m_waitingToGoDown.begin(), m_waitingToGoDown.end());
+}
+
+void ElevatorController::mergeMaxWaitingToGoDown()
+{
+	if (m_waitingToGoDown.empty())
+	{
+		return;
+	}
+	auto it = max(m_destinations1.begin(), m_destinations1.end());
+	m_waitingToGoDown.erase(*it);
+	m_destinations1.insert(*it);
+}
+
+void ElevatorController::mergeMinWaitingToGoUp()
+{
+	if (m_waitingToGoUp.empty())
+	{
+		return;
+	}
+	auto it = min(m_destinations1.begin(), m_destinations1.end());
+	m_waitingToGoUp.erase(*it);
+	m_destinations1.insert(*it);
+}
 
 
--- Examples/Elevator/Controller/ElevatorPetriNet.cpp
+++ Examples/Elevator/Controller/ElevatorPetriNet.cpp
@@ -25,32 +25,288 @@ ElevatorPetriNet::ElevatorPetriNet(std::shared_ptr<ElevatorController> ptrContro
 {
 	using namespace std;
 
+	//Create places
+
+	//inputs
+	addPlace("CloseDoors", 0, nullptr, nullptr, true);
+	addPlace("OpenDoors", 0, nullptr, nullptr, true);
+	addPlace("CallButtonUp", 0, nullptr, nullptr, true);
+	addPlace("CallButtonDown", 0, nullptr, nullptr, true);
+	addPlace("DestinationButton", 0, nullptr, nullptr, true);
+
+	//controller places
+	addPlace("Stopped", 1, nullptr, nullptr);
+	addPlace("Moving", 0, nullptr, nullptr);
+	addPlace("DoorsOpened", 1, nullptr, nullptr);
+	addPlace("DoorsClosed", 0, nullptr, nullptr);
+	addPlace("ArrivedFloor", 0, nullptr, nullptr);
+	addPlace("HasDestination", 0, nullptr, nullptr);
+
+	addPlace("GoingUp", 0, nullptr, nullptr);
+	addPlace("GoingDown", 0, nullptr, nullptr);
+	addPlace("AddToTravel", 0, nullptr, nullptr); //needs function
+	addPlace("AddToNextTravel", 0, nullptr, nullptr); //needs function
+	addPlace("WaitToGoUp", 0, nullptr, nullptr); 
+	addPlace("WaitToGoDown", 0, nullptr, nullptr);
+	addPlace("ArrivedDestination", 0, nullptr, nullptr);
+	addPlace("RemoveFromList", 0, nullptr, nullptr); //needs function
+	addPlace("SwapLists", 0, nullptr, nullptr); //needs function
+	addPlace("ProcessGoUp", 0, nullptr, nullptr); //needs function
+	addPlace("ProcessGoDown", 0, nullptr, nullptr); //needs function
+	addPlace("Ready", 1, nullptr, nullptr);
+	addPlace("Aux1", 0, nullptr, nullptr);
+	addPlace("Aux2", 0, nullptr, nullptr);
+	addPlace("Aux3", 0, nullptr, nullptr);
+	addPlace("P1", 0, nullptr, nullptr);
+	addPlace("P2", 0, nullptr, nullptr);
+
+	//simulation
+	addPlace("IncreaseFloor", 0, nullptr, nullptr); //needs function
+	addPlace("DecreaseFloor", 0, nullptr, nullptr); //needs function
+
+	
+	//Create transitions
+
+	createTransition({"CloseDoors", "Stopped", "DoorsOpened"}, {"Stopped", "DoorsClosed"},
+		{}
+	);
+
+	createTransition({"OpenDoors", "Stopped", "DoorsClosed"}, {"Stopped", "DoorsOpened"},
+		{}
+	);
+
+	createTransition({"HasDestination", "Stopped", "DoorsClosed" }, {"HasDestination", "DoorsClosed", "Moving"},
+		{}
+	);
+
+	createTransition({"ArrivedDestination", "Moving"}, {"Stopped", "OpenDoors", "Ready"},
+		{}
+	);
+
+	/////////////////
+	//simulation
+	createTransition({"Moving", "IncreaseFloor", "GoingUp"}, {"Moving", "OpenDoors"},
+		{}
+	);
+	
+	createTransition({"IncreaseFloor"}, {"IncreaseFloor"},
+		{} //needs function
+	);
+
+	createTransition({"IncreaseFloor" }, {"ArrivedDestination"},
+		{} //needs function
+	);
+
+	createTransition({"Moving", "DecreaseFloor", "GoingDown"}, {"Moving", "OpenDoors"},
+		{}
+	);
+
+	createTransition({"DecreaseFloor"}, {"DecreaseFloor"},
+		{} //needs function
+	);
+
+	createTransition({"DecreaseFloor"}, {"ArrivedDestination"},
+		{} //needs function
+	);
+	////////////
+
+	//////////////////
+	//Arriving a floor.	
+
+	createTransition({"Ready", "ArrivedFloor"}, { "Ready" },
+		{} //needs function: floor not in list
+	); //arrived floor not in list
+
+	createTransition({"Ready", "ArrivedFloor", "HasDestination"}, { "RemoveFromList" },
+		{} //needs function: floor in list
+	); //arrived floor in list
+
+	createTransition({"RemoveFromList"}, {"ArrivedDestination", "HasDestination"},
+		{} //needs function: list not empty
+	); //arrived floor not in list
+
+	createTransition({"RemoveFromList"}, {"Swap"},
+		{} //needs function: list not empty
+	); //arrived floor in list
+
+	createTransition({"Swap", "GoingUp"}, {"ProcessGoDown"},
+		{}
+	); 
+
+	createTransition({"Swap", "GoingDown"}, {"ProcessGoUp"},
+		{}
+	);
+
+	createTransition({"ProcessGoDown"},	{"ArrivedDestination"},
+		{} //needs function: list is empty
+	);
+
+	createTransition({"ProcessGoUp"}, {"ArrivedDestination"},
+		{} //needs function: list is empty
+	);
+
+	createTransition({"ProcessGoDown"},	{"ArrivedDestination", "HasDestination", "GoingDown"},
+		{} //needs function: list is not empty
+	);
+
+	createTransition({"ProcessGoUp"}, {"ArrivedDestination", "HasDestination", "GoingUp"},
+		{} //needs function: list is not empty
+	);
+
+	//////////////////
+	
+
+	//////////////////
+	//Pressing a floor button inside the elevator.	
+
+	createTransition({"DestinationButton", "Ready"}, {"P1"},
+		{}, //needs function: marked floor != current floor
+		{"GoingUp", "GoingDown"}
+	);
+
+	createTransition({"DestinationButton", "Ready", "GoingUp"}, {"Aux1", "GoingUp"},
+		{} //needs function: marked floor != current floor
+	);
+
+	createTransition({"DestinationButton", "Ready", "GoingDown"}, {"Aux2", "GoingDown"},
+		{} //needs function: marked floor != current floor
+	);
+	
+	//has no destination
+
+	createTransition({ "P1" }, {"P2", "GoingUp" },
+		{} //needs function: pressed floor button > current floor
+	);
+
+	createTransition({ "P1" }, { "P2", "GoingDown" },
+		{} //needs function: pressed floor button < current floor
+	);
+
+	createTransition({ "P2" }, { "AddToTravel" },
+		{} 
+	);
+
+	//is moving up
+
+	createTransition({"Aux1"}, {"AddToTravel"},
+		{} //needs function: pressed floor button > current floor
+	);
+
+	createTransition({"Aux1"}, { "AddToNextTravel"},
+		{} //needs function: pressed floor button < current floor
+	);
+
+	//is moving down
+
+	createTransition({ "Aux2" }, { "AddToTravel" },
+		{} //needs function: pressed floor button < current floor
+	);
+
+	createTransition({ "Aux2" }, { "AddToNextTravel" },
+		{} //needs function: pressed floor button > current floor
+	);
+
+
+	createTransition({ "AddToNextTravel" }, { "Ready" },
+		{} 
+	);
+
+	createTransition({ "AddToTravel", "HasDestination" }, { "Ready", "HasDestination" },
+		{}
+	);
+
+	createTransition({ "AddToTravel" }, { "Ready" },
+		{},
+		{ "HasDestination" }
+	);
+
+	//////////////////
+
+
+	//////////////////
+	//Calling the elevator to go up.	
+
+	createTransition({ "CallButtonUp", "Ready", "GoingUp" }, { "Aux2", "GoingUp" },
+		{} //needs function: marked floor != current floor
+	);
+
+	createTransition({ "CallButtonUp", "Ready", "GoingDown" }, { "AddToNextTravel", "GoingDown" },
+		{} //needs function: marked floor != current floor
+	);
+
+	createTransition({ "CallButtonUp", "Ready" }, { "P1" },
+		{}, //needs function: marked floor != current floor
+		{ "GoingUp", "GoingDown" }
+	);
+
+	createTransition({ "Aux2" }, { "AddToTravel" },
+		{} //needs function: floor > current floor
+	);
+
+	createTransition({ "Aux2" }, { "WaitToGoUp" },
+		{} //needs function: floor < current floor
+	);
+
+	createTransition({ "WaitToGoUp" }, { "Ready" },
+		{} 
+	);
+
+	//////////////////
+
+	//////////////////
+	//Calling the elevator to go down.	
+
+	createTransition({ "CallButtonDown", "Ready", "GoingDown" }, { "Aux3", "GoingDown" },
+		{} //needs function: marked floor != current floor
+	);
+
+	createTransition({ "CallButtonDown", "Ready", "GoingUp" }, { "AddToNextTravel", "GoingUp" },
+		{} //needs function: marked floor != current floor
+	);
+
+	createTransition({ "CallButtonDown", "Ready" }, { "P1" },
+		{}, //needs function: marked floor != current floor
+		{ "GoingUp", "GoingDown" }
+	);
+
+	createTransition({ "Aux3" }, { "AddToTravel" },
+		{} //needs function: floor > current floor
+	);
+
+	createTransition({ "Aux3" }, { "WaitToGoDown" },
+		{} //needs function: floor < current floor
+	);
+
+	createTransition({ "WaitToGoDown" }, { "Ready" },
+		{}
+	);
+	//////////////////
 
 }
 
 void ElevatorPetriNet::closeDoors()
 {
-
+	incrementInputPlace("CloseDoors");
 }
 
 void ElevatorPetriNet::openDoors()
 {
-
+	incrementInputPlace("OpenDoors");
 }
 
 void ElevatorPetriNet::callButtonUp()
 {
-
+	incrementInputPlace("CallButtonUp");
 }
 
 void ElevatorPetriNet::callButtonDown()
 {
-
+	incrementInputPlace("CallButtonDown");
 }
 
 void ElevatorPetriNet::destinationButton()
 {
-
+	incrementInputPlace("DestinationButton");
 }
 
 //void Controller::MenuStateMachine::pressA()
