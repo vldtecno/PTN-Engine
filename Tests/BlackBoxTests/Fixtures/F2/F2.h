e9c8b7fc3b3539fdfe1cf834088a49c2b9e907c2
Eduardo Valgôde <vldtecno@sapo.pt>
2018-07-01 22:13:58 +0200

    Small correction on header's dates.

--- Tests/BlackBoxTests/Fixtures/F2/F2.h
+++ Tests/BlackBoxTests/Fixtures/F2/F2.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of PTN Engine
  *
- * Copyright (c) 2017 Eduardo Valgôde
+ * Copyright (c) 2018 Eduardo Valgôde
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
